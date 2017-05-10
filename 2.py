4c4a1bf87af620153e5a4723321672d327b11ff3
Ashton Honnecke <ashton@havenly.com>
2017-05-10 15:36:34 -0600

    now with more snake

---
+++ 2.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+total = 0
+upper = 4000000
+#upper = 100
+num = 1
+last = 1
+laster = 0
+
+while (num < upper):
+    laster = last
+    last = num
+    num = laster + last
+    #this print makes fibonacci numbers
+    #print num
+    if(num <= upper and num % 2 == 0):
+        total += num
+#        print num
+
+print total;
+
+#✔ 11:24 ~/Code/euler [master|✚ 2…4] $ python 2.py
+#4613732
