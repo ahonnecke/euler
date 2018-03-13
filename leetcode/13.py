238eb474ab43d79b3da2f051c2c2a5e3d0d00fc5
Ashton Honnecke <ashton@pixelstub.com>
2018-03-13 02:01:49 -0600

    Adding descriptions

--- leetcode/13.py
+++ leetcode/13.py
@@ -1,3 +1,9 @@
+# https://leetcode.com/problems/roman-to-integer/description/
+
+# Given a roman numeral, convert it to an integer.
+
+# Input is guaranteed to be within the range from 1 to 3999.
+
 class Solution:
     numerals = [
         {"dec": 1000, "rom": "M"},
